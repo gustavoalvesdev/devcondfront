bf582e3051a51321a6bfe3695aad151c67d052e0
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-08 16:02:19 -0300

    Avisos 1

--- src/services/api.js
+++ src/services/api.js
@@ -52,6 +52,12 @@ export default () => {
         localStorage.removeItem('token');
 
         return json;
+      },
+
+      getWall: async () => {
+        let token = localStorage.getItem('token');
+        let json = await request('get', '/walls', {}, token);
+        return json;
       }
     };
 }
