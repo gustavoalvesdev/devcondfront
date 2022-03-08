cc91d3551129e6a562c0496a04b06295629f0fe2
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-03-08 18:20:22 -0300

    Avisos 3

--- src/views/Wall.js
+++ src/views/Wall.js
@@ -1,5 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { CButton, CButtonGroup, CCard, CCardBody, CCardHeader, CCol, CDataTable, CRow } from '@coreui/react';
+import { 
+    CButton, 
+    CButtonGroup, 
+    CCard, 
+    CCardBody, 
+    CCardHeader, 
+    CCol, 
+    CDataTable, 
+    CRow, 
+    CModal,
+    CModalHeader, 
+    CModalBody, 
+    CModalFooter ,
+    CFormGroup,
+    CLabel,
+    CTextarea,
+    CInput
+} from '@coreui/react';
 import CIcon from '@coreui/icons-react';
 
 import useApi from '../services/api';
@@ -9,6 +26,9 @@ export default () => {
 
   const [loading, setLoading] = useState(true);
   const [list, setList] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [modalTitleField, setModalTitleField] = useState('');
+  const [modalBodyField, setModalBodyField] = useState('');
 
   const fields= [
     {label: 'Título', key: 'title'},
@@ -31,43 +51,83 @@ export default () => {
     }
   }
 
+  const handleCloseModal = () => {
+    setShowModal(false);
+  }
+
+  const handleEditButton = (index) => {
+    setShowModal(true);
+  }
+
   return (
-    <CRow>
-      <CCol>
-        <h2>Mural de Avisos</h2>
+    <>
+      <CRow>
+        <CCol>
+          <h2>Mural de Avisos</h2>
 
-        <CCard>
-          <CCardHeader>
-            <CButton color="primary">
-              <CIcon name="cil-check" /> Novo Aviso
-            </CButton>
-          </CCardHeader>
-          <CCardBody>
-            <CDataTable
-              items={list}
-              fields={fields}
-              loading={loading}
-              noItemsViewSlot=" "
-              hover
-              striped
-              bordered
-              pagination
-              itemsPerPage={5}
-              scopedSlots={{
-                'actions': (item, index) => (
-                  <td>
-                    <CButtonGroup>
-                      <CButton color="info">Editar</CButton>
-                      <CButton color='danger'>Excluir</CButton>
-                    </CButtonGroup>
-                  </td>
+          <CCard>
+            <CCardHeader>
+              <CButton color="primary">
+                <CIcon name="cil-check" /> Novo Aviso
+              </CButton>
+            </CCardHeader>
+            <CCardBody>
+              <CDataTable
+                items={list}
+                fields={fields}
+                loading={loading}
+                noItemsViewSlot=" "
+                hover
+                striped
+                bordered
+                pagination
+                itemsPerPage={5}
+                scopedSlots={{
+                  'actions': (item, index) => (
+                    <td>
+                      <CButtonGroup>
+                        <CButton color="info" onClick={() => handleEditButton(index)}>Editar</CButton>
+                        <CButton color='danger'>Excluir</CButton>
+                      </CButtonGroup>
+                    </td>
 
-                )
-              }}
-            />
-          </CCardBody>
-        </CCard>
-      </CCol>
-    </CRow>
+                  )
+                }}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+
+
+      <CModal show={showModal} onClose={handleCloseModal}>
+        <CModalHeader closeButton>Editar Aviso</CModalHeader>
+        <CModalBody>
+            <CFormGroup>
+                <CLabel htmlFor="modal-title">Título do Aviso</CLabel>
+                <CInput 
+                    type="text"
+                    id="modal-title"
+                    placeholder="Digite um título para o aviso"
+                    value={modalTitleField}
+                    onChange={e => setModalTitleField(e.target.value)}
+                />
+            </CFormGroup>
+            <CFormGroup>
+                <CLabel htmlFor="modal-body">Corpo do Aviso</CLabel>
+                <CTextarea 
+                    id="modal-body"
+                    placeholder="Digite o conteúdo do aviso"
+                    value={modalBodyField}
+                    onChange={e => setModalBodyField(e.target.value)}
+                />
+            </CFormGroup>
+        </CModalBody>
+        <CModalFooter>
+            <CButton color="primary">Salvar</CButton>
+            <CButton color="secondary">Cancelar</CButton>
+        </CModalFooter>
+      </CModal>
+    </>
   );
 }
